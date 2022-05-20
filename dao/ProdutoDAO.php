49ffd91794357e59b2688355d744d71b18860379
Sávio Chaves Cardoso da Silva <savio.silva@EDUC.NET>
2022-05-20 12:04:46 -0300

    ajustes

--- dao/ProdutoDAO.php
+++ dao/ProdutoDAO.php
@@ -21,18 +21,6 @@ class ProdutoDAO {
         }
     }
 
-/*     public function findByIdIn( $ids ) {
-try {
-$sql  = "SELECT * FROM tb_produto WHERE id IN ({$ids})";
-$stmt = $this->pdo->prepare( $sql );
-$stmt->execute();
-$produtos = $stmt->fetchAll( PDO::FETCH_ASSOC );
-return $produtos;
-} catch ( PDOException $e ) {
-echo 'Erro ao listar os produtos: ', $e->getMessage();
-}
-} */
-
     public function findById( $id ) {
         try {
             $sql  = "SELECT * FROM tb_produto WHERE id = ?";
